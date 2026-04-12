20f5f24d92d2c0b7b4bcccc4be16104b5c14c37e
Gayathri Kunduru <gayathrikunduru1230@gmail.com>
2026-04-12 21:40:05 +0530

    removed service worker for vercel fix

--- src/index.js
+++ src/index.js
@@ -11,18 +11,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-// 🔴 Disable service worker during development
-// (Prevents cache bugs)
-
-if (false) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then(() => console.log("✅ SW Registered"))
-      .catch((err) => console.log("❌ SW Error", err));
-  });
-}
